40b3bbd845f9e1c1cdda348b658a24c104ea2126
Aaryan Goswami <114975960+AaryanGoswami04@users.noreply.github.com>
2023-08-02 21:02:59 +0530

    Create 3D_CherryPickup.cpp

---
+++ 3D_CherryPickup.cpp
@@ -0,0 +1,74 @@
+// https://leetcode.com/problems/cherry-pickup-ii/
+/*You are given a rows x cols matrix grid representing a field of cherries where grid[i][j] represents the number of cherries that you can collect from the (i, j) cell.
+
+You have two robots that can collect cherries for you:
+
+Robot #1 is located at the top-left corner (0, 0), and
+Robot #2 is located at the top-right corner (0, cols - 1).
+Return the maximum number of cherries collection using both robots by following the rules below:
+
+From a cell (i, j), robots can move to cell (i + 1, j - 1), (i + 1, j), or (i + 1, j + 1).
+When any robot passes through a cell, It picks up all cherries, and the cell becomes an empty cell.
+When both robots stay in the same cell, only one takes the cherries.
+Both robots cannot move outside of the grid at any moment.
+Both robots should reach the bottom row in grid.*/
+
+//Recursion: TC:O(3^N * 3^N); SC:O(N)
+//Since both robots reach a row simultaneously, only one state is used to denote row idx
+int rec(int r, int c1, int c2, int n, int m, vector<vector<int>>& grid){
+        if(c1 < 0 or c2 < 0 or c1 >= m or c2 >= m) return INT_MIN;
+
+        if(r == n-1){ //Both robots have reached last row
+            if(c1 == c2) return grid[r][c1];
+            else return grid[r][c1] + grid[r][c2];
+        }
+
+        //explore all possible paths of both robots simultaneously
+        int maxi=0;
+
+        for(int dc1=-1; dc1<=1; dc1++){
+
+            for(int dc2=-1; dc2<=1; dc2++){
+            
+                int val = (c1 == c2) ? grid[r][c1] : grid[r][c1]+grid[r][c2]; //Determining if both robots are on the same cell or not
+                val += rec(r+1, c1 + dc1, c2 + dc2, n, m, grid);
+
+                maxi = max(maxi, val);
+            }
+        }
+        return maxi;
+  }
+
+//Memoization: TC:O(N*M*M); SC:O(N)
+int rec(int r, int c1, int c2, int n, int m, vector<vector<vector<int>>>& dp, vector<vector<int>>& grid){
+        if(c1 < 0 or c2 < 0 or c1 >= m or c2 >= m) return -1e8;
+
+        if(r == n-1){ //Both robots have reached last row
+            if(c1 == c2) return grid[r][c1];
+            else return grid[r][c1] + grid[r][c2];
+        }
+
+        if(dp[r][c1][c2] != 0) return dp[r][c1][c2];
+
+        //explore all possible paths of both robots simultaneously
+        int maxi=0;
+
+        for(int dc1=-1; dc1<=1; dc1++){
+
+            for(int dc2=-1; dc2<=1; dc2++){
+            
+                int val = (c1 == c2) ? grid[r][c1] : grid[r][c1]+grid[r][c2]; //Determining if both robots are on the same cell or not
+                val += rec(r+1, c1 + dc1, c2 + dc2, n, m, dp, grid);
+
+                maxi = max(maxi, val);
+            }
+        }
+        return dp[r][c1][c2] = maxi;
+    }
+
+int cherryPickup(vector<vector<int>>& grid) {
+      int n=grid.size(), m=grid[0].size();
+      vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(m, 0)));
+
+      return rec(0, 0, m-1, n, m, dp, grid);
+  }
