fbcfa32fe165ab56c3365cdad70dd52a13d8cbb8
Aaryan Goswami <114975960+AaryanGoswami04@users.noreply.github.com>
2023-08-10 21:31:56 +0530

    Create Print_LCS.cpp

---
+++ Print_LCS.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+//dp[i][j] stores length of LCS in str1[i,m-1] to str2[j,n-1]
+int main() {
+   string s1="Atbcghi", s2="Bbctghi";
+   int m=6, n=6;
+  
+   vector<vector<int>> dp(m+1, vector<int>(n+1,-1));
+
+  //Tabulation: TC:O(M*N); SC:O(M*N)
+  //set base cases
+    for(int i=0; i<=m; i++) dp[i][n] = 0;
+    for(int j=0; j<=n; j++) dp[m][j] = 0;
+
+    for(int i=m-1; i>=0; i--){
+      for(int j=n-1; j>=0; j--){
+        if(s1[i] == s2[j]) dp[i][j] = 1 + dp[i+1][j+1];
+        
+        else dp[i][j] = max(dp[i+1][j], dp[i][j+1]);
+      }
+    }
+
+    string ans;
+  
+    int i=0,j=0;
+    //TC:O(M+N); SC:O(1)
+    while(i<m+1 and j<n+1){
+      if(s1[i] == s2[j]){
+        ans.push_back(s1[i]);
+        i++;
+        j++;
+      }
+
+      else if(dp[i+1][j] > dp[i][j+1]) i++;
+        
+      else j++;
+    }
+    cout<<ans<<endl;
+}
