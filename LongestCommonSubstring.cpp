7b07db881bd3f8926ec29302085e1bec01f8e822
Aaryan Goswami <114975960+AaryanGoswami04@users.noreply.github.com>
2023-08-11 12:05:13 +0530

    Update LongestCommonSubstring.cpp

--- LongestCommonSubstring.cpp
+++ LongestCommonSubstring.cpp
@@ -25,3 +25,27 @@ int lcs(string &s1, string &s2){
         }
        return ans;
 }
+
+//Space optimisation: SC:O(M)
+int lcs(string &s1, string &s2){
+    int n=s1.size();
+    int m=s2.size();
+    int ans=0;
+
+    vector<int> prev(m+1, 0), curr(m+1, 0);
+          
+    for(int i=1; i<=n; i++)
+    {
+        for(int j=1; j<=m; j++)
+        {
+            if (s1[i - 1] == s2[j - 1]) {
+                curr[j] = 1 + prev[j - 1];
+                ans = max(ans, curr[j]);
+            }
+
+            else curr[j] = 0;
+        }
+        prev = curr;
+    }
+    return ans;
+}
