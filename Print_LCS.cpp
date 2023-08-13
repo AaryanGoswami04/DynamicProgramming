6936cb036d243cdf5e94a9c686c6b6e5d54cf6ee
Aaryan Goswami <114975960+AaryanGoswami04@users.noreply.github.com>
2023-08-13 10:46:33 +0530

    Update Print_LCS.cpp

--- Print_LCS.cpp
+++ Print_LCS.cpp
@@ -7,13 +7,9 @@ int main() {
    string s1="Atbcghi", s2="Bbctghi";
    int m=6, n=6;
   
-   vector<vector<int>> dp(m+1, vector<int>(n+1,-1));
+   vector<vector<int>> dp(m+1, vector<int>(n+1,0));
 
   //Tabulation: TC:O(M*N); SC:O(M*N)
-  //set base cases
-    for(int i=0; i<=m; i++) dp[i][n] = 0;
-    for(int j=0; j<=n; j++) dp[m][j] = 0;
-
     for(int i=m-1; i>=0; i--){
       for(int j=n-1; j>=0; j--){
         if(s1[i] == s2[j]) dp[i][j] = 1 + dp[i+1][j+1];
