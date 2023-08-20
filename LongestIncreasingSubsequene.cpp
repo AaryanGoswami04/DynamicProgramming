0b7c6006d6d7ac4ffda12c9d0b437dddf41306bb
Aaryan Goswami <114975960+AaryanGoswami04@users.noreply.github.com>
2023-08-20 16:24:12 +0530

    Update LongestIncreasingSubsequene.cpp

--- LongestIncreasingSubsequene.cpp
+++ LongestIncreasingSubsequene.cpp
@@ -50,3 +50,19 @@ int lengthOfLIS(vector<int>& nums) {
         }
         return Next[0];
 }
+
+//Optimised approach: TC:O(N^2); SC:O(N)
+//dp[i] stores length of LIS ending at index i
+int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 1;
+        vector<int> dp(n,1);
+
+        for(int i = 0; i < n; i++){
+            for(int prev = 0; prev < i; prev++){
+                if(nums[prev] < nums[i]) dp[i] = max(dp[i], 1 + dp[prev]);
+                ans = max(ans, dp[i]);
+            }
+        }
+        return ans;
+}
