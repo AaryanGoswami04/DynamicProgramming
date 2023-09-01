56045ce4076e3885036a2116a6326e28e7f66af6
Aaryan Goswami <114975960+AaryanGoswami04@users.noreply.github.com>
2023-09-01 18:17:53 +0530

    Update MaxtrixChainMultiplication_PartitionDP.cpp

--- MaxtrixChainMultiplication_PartitionDP.cpp
+++ MaxtrixChainMultiplication_PartitionDP.cpp
@@ -1,6 +1,6 @@
 //Given the dimension of a sequence of matrices in an array arr[], where the dimension of the ith matrix is (arr[i-1] * arr[i]), the task is to find the most efficient way to multiply these matrices together such that the total number of element multiplications is minimum.
 
-//Memoization: TC:O(N^3); SC:O(N)
+//Memoization: TC:O(N^3); SC:O(N^2 + N)
 //dp[i][j] stores the min operartions to multiply ith matrix to jth matrix
 int rec(int i, int j, vector<int> &arr, vector<vector<int>>& dp){
     if(i == j) return 0;
@@ -14,4 +14,24 @@ int rec(int i, int j, vector<int> &arr, vector<vector<int>>& dp){
     }
 
     return dp[i][j] = mini;
+}
+
+//Tabulation: TC:O(N^3); SC:O(N^2)
+int matrixMultiplication(vector<int> &arr, int N)
+{
+    int dp[N][N]={0};
+
+    for(int i = N-1 ; i >=1 ; i--)
+    {
+        for(int j = i+1; j <= N-1; j++)
+        {
+            int mini=1e9;
+            for(int k=i; k<=j-1; k++){
+                int steps = arr[i-1]*arr[k]*arr[j] + dp[i][k] + dp[k+1][j];
+                mini = min(steps, mini);
+            }
+            dp[i][j] = mini;
+        }
+    }
+    return dp[1][N-1];
 }
